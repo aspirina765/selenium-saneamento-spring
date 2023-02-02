0efca494505ac96b70145e3ee9c7d170a4ab5514
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-02-02 02:25:53 -0300

    Escrevendo no arquivo

--- src/main/java/net/cloudtecnologia/client/Chrome.java
+++ src/main/java/net/cloudtecnologia/client/Chrome.java
@@ -4,17 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Chrome {
 
-    private static String URL_LOGIN = "http://wlcontabilidade.sytes.net:8080/wlgestao/login.jsf";
-    private static String URL_SANEAMENTO_PRODUTOS = "http://wlcontabilidade.sytes.net:8080/wlgestao/paginas/saneamento/commom/produtossaneamento.jsf";
-    private static String USER = "anilson-asj";
-    private static String PASS = "115200";
-
+    private static final String URL_LOGIN = "http://wlcontabilidade.sytes.net:8080/wlgestao/login.jsf";
+    private static final String URL_SANEAMENTO_PRODUTOS = "http://wlcontabilidade.sytes.net:8080/wlgestao/paginas/saneamento/commom/produtossaneamento.jsf";
+    private static final String USER = "anilson-asj";
+    private static final String PASS = "115200";
+    private static final String CAMINHO_TXT = "C:\\Users\\thiago.melo\\Desktop\\SANEAMENTO.txt";
 
     public WebDriver retornarNaveghador() {
         System.setProperty("webdriver.chrome.driver",
@@ -44,40 +48,55 @@ public class Chrome {
         ler.next();
         int totalRegistros = 0;
         int max = 4644;
-        for (int i = 1; i <= max; i++) {
-            String info = navegador.findElement(By.xpath("//*[@id=\"formSaneamento:listaProdutosEmpresa_paginator_top\"]/span[1]")).getText();
-            System.out.println("Processando HTML:");
-            System.out.println(info);
-            //
-            List<WebElement> lista = navegador.findElement(By.className("ui-datatable-tablewrapper"))
-                    .findElements(By.tagName("tr"));
-            System.out.println("");
-            System.out.println("  INFO: ");
-            System.out.println("");
-            int cont = 0;
-            for (WebElement elemento : lista) {
-                if (cont > 0) {
-                    totalRegistros++;
-                    List<WebElement> colunas = elemento.findElements(By.tagName("td"));
-                    //
-                    try {
-                        String linha = "BARRA:" + colunas.get(4).getText() + " # DESCRIÇÃO:" + colunas.get(5).getText() +
-                                " # NCM:" + colunas.get(6).getText() + " # CST: " + colunas.get(7).getText() +
-                                " # ALIQ C: " + colunas.get(8).getText() + " # ALIQ R: " + colunas.get(9).getText();
-                        System.out.println(totalRegistros + " | " + linha);
-                    } catch (Exception e) {
-                        System.out.println("***Erro***");
+        //
+        try {
+            BufferedWriter escreve = new BufferedWriter(new FileWriter((CAMINHO_TXT), true));
+            for (int i = 1; i <= max; i++) {
+                String info = navegador.findElement(By.xpath("//*[@id=\"formSaneamento:listaProdutosEmpresa_paginator_top\"]/span[1]")).getText();
+                System.out.println("Processando HTML:");
+                System.out.println(info);
+                //
+                List<WebElement> lista = navegador.findElement(By.className("ui-datatable-tablewrapper"))
+                        .findElements(By.tagName("tr"));
+                System.out.println("");
+                System.out.println("  INFO: ");
+                System.out.println("");
+                int cont = 0;
+                for (WebElement elemento : lista) {
+                    if (cont > 0) {
+                        totalRegistros++;
+                        List<WebElement> colunas = elemento.findElements(By.tagName("td"));
+                        //
+                        try {
+                            String linha = totalRegistros + " # BARRA:" + colunas.get(4).getText() +
+                                    " # DESCRIÇÃO:" + colunas.get(5).getText() +
+                                    " # NCM:" + colunas.get(6).getText() + " # CST: " + colunas.get(7).getText() +
+                                    " # ALIQ C: " + colunas.get(8).getText() + " # ALIQ R: " + colunas.get(9).getText();
+                            //
+                            escreve.write(linha.trim());
+                            escreve.newLine();
+                            escreve.flush();
+                            System.out.println(linha);
+                            //
+                        } catch (Exception e) {
+                            System.out.println("***Erro ao obter informações colunas***");
+                        }
                     }
+                    cont++;
                 }
-                cont++;
-            }
-            navegador.findElement(By.xpath("//*[@id=\"formSaneamento:listaProdutosEmpresa_paginator_top\"]/a[3]")).click();
-            System.out.println("");
-            System.out.println("Clicou na ABA: " + (i + 1));
-            System.out.println("");
-            System.out.println("---------------------------------------------------------------");
-            System.out.println("");
-            aguardarSeconds(10);
+                navegador.findElement(By.xpath("//*[@id=\"formSaneamento:listaProdutosEmpresa_paginator_top\"]/a[3]")).click();
+                System.out.println("");
+                System.out.println("Clicou na ABA: " + (i + 1));
+                System.out.println("");
+                System.out.println("---------------------------------------------------------------");
+                System.out.println("");
+                aguardarSeconds(10);
+            }//fim do laço
+            escreve.close();
+            JOptionPane.showMessageDialog(null, "Processo concluído!!!!");
+        } catch (IOException e) {
+            System.out.println("***Erro ao Escrever no Arquivo!***");
+            e.printStackTrace();
         }
     }
 
